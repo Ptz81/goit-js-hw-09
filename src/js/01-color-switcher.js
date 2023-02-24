b41f451a2bb0a142c1226eb52b80d570d08437fe
Taras Papka <taras.papka@protonmail.com>
2023-02-24 23:33:14 +0200

    added comments

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,23 +1,28 @@
+//знайти елементи
 const btnStartElement = document.querySelector('[data-start]');
 const btnStopElement = document.querySelector('[data-stop]');
 let timerId = null;
 const body = document.body;
 
+// слухач при натисканні кнопки старт - встановлює інтервал, змінює колір відповідно до рандомної функції
 btnStartElement.addEventListener("click", () => {
   timerId = setInterval(() => {
    body.style.backgroundColor = getRandomHexColor();
   }, 1000);
+  //робить кнопку старт не активною, а кнопку стоп активна
   btnStartElement.disabled = true;
   btnStopElement.disabled = false;
 });
 
+// слухач при натисканні кнопки стоп - очищує інтервал
 btnStopElement.addEventListener("click", () => {
   clearInterval(timerId);
+  //робить кнопку стоп не активною, а кнопку старт активною
    btnStartElement.disabled = false;
   btnStopElement.disabled = true;
  });
 
-
+//рандомна фукнція підбору кольору
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
