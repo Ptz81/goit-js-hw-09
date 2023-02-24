fbd548cefb2c50a5f541113843323f136f7a6dfb
Taras Papka <taras.papka@protonmail.com>
2023-02-24 23:29:31 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const btnStartElement = document.querySelector('[data-start]');
+const btnStopElement = document.querySelector('[data-stop]');
+let timerId = null;
+const body = document.body;
 
+btnStartElement.addEventListener("click", () => {
+  timerId = setInterval(() => {
+   body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  btnStartElement.disabled = true;
+  btnStopElement.disabled = false;
+});
+
+btnStopElement.addEventListener("click", () => {
+  clearInterval(timerId);
+   btnStartElement.disabled = false;
+  btnStopElement.disabled = true;
+ });
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
