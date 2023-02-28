f847ea9c34bd83d8e9345888ed2340c6dc8561d3
Taras Papka <taras.papka@protonmail.com>
2023-02-28 23:39:59 +0200

    found elements on document

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,11 @@
+//знайти елементи
+const formElem = document.querySelector('.form');
+const delayInputElem = document.querySelector('input[name="delay"]');
+const stepInputElem = document.querySelector('input[name="step"]');
+const amountInputElem = document.querySelector('input[name="amount"]');
+
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
