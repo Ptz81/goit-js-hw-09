f773514c2ea4aedb4a4cc992d0a586b124551153
Taras Papka <taras.papka@protonmail.com>
2023-03-01 15:38:15 +0200

    added listener, promise, comments

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,20 @@
+import Notiflix from 'notiflix';
+
+/*
+Напиши скрипт, який на момент сабміту форми викликає функцію
+createPromise(position, delay) стільки разів, скільки ввели в поле amount.
+Під час кожного виклику передай їй номер промісу (position),
+що створюється, і затримку, враховуючи першу затримку (delay),
+введену користувачем, і крок (step).
+Доповни код функції createPromise таким чином,
+щоб вона повертала один проміс, який виконується або відхиляється через
+delay часу. Значенням промісу повинен бути об'єкт, в якому будуть
+властивості position і delay зі значеннями однойменних параметрів.
+Використовуй початковий код функції для вибору того, що потрібно
+зробити з промісом - виконати або відхилити.
+*/
+
+
 //знайти елементи
 const formElem = document.querySelector('.form');
 const delayInputElem = document.querySelector('input[name="delay"]');
@@ -14,3 +31,16 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+formElem.addEventListener('input', (e) => {
+  e.target
+  createPromise(stepInputElem, delayInputElem)
+})
+
+
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    Notiflix.Notify.warning(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
